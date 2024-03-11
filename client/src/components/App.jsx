1062fbd99493e23ce06d4c101f4ab16fcdee415c
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2024-03-11 17:28:05 +0530

    Navbar-Done

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -16,6 +16,9 @@ import Bookings from '../pages/Bookings';
 import SingleBooking from '../pages/SingleBooking';
 
 import { UserContextProvider } from "../context/UserContext";
+import Places from '../pages/Places';
+import About from '../pages/About';
+import Contact from '../pages/Contact';
 
 
 axios.defaults.baseURL = 'http://localhost:4000/api';
@@ -23,24 +26,29 @@ axios.defaults.withCredentials = true;
 
 const App = () => {
   return (
-    <UserContextProvider>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/profile/places" element={<ProfilePlaces />} />
-          <Route path="/profile/places/new" element={<AddProfilePlaces />} />
-          <Route path="/profile/places/:id" element={<AddProfilePlaces />} />
-          <Route path="/places/:id" element={<SinglePlace />} />
-          <Route path="/profile/bookings" element={<Bookings />} />
-          <Route path="/profile/bookings/:id" element={<SingleBooking />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </UserContextProvider>
+    <div className='app'>
+      <UserContextProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile/places" element={<ProfilePlaces />} />
+            <Route path="/profile/places/new" element={<AddProfilePlaces />} />
+            <Route path="/profile/places/:id" element={<AddProfilePlaces />} />
+            <Route path="/places/:id" element={<SinglePlace />} />
+            <Route path="/profile/bookings" element={<Bookings />} />
+            <Route path="/profile/bookings/:id" element={<SingleBooking />} />
+            <Route path="/places" element={<Places />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </UserContextProvider>
+    </div>
   )
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import logo from "../assets/icon.svg"
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const { user } = useContext(UserContext);
+
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <Link to={'/'} className="left">
+        <img src={logo} alt="logo" />
+        <p>aircnc</p>
+      </Link>
+      <div className="center">
+        <Link to={'/places'}>Places</Link>
+        <Link to={'/about'}>About</Link>
+        <Link to={'/contact'}>Contact</Link>
+      </div>
+      <div className="right">
+        {
+          user ? (
+            <Link className='rightbtn' to={'/profile'}>
+              <p>{user.name}</p>
+              <i class="fa-regular fa-user"></i>
+            </Link>
+          ) : (
+            <Link className='rightbtn' to={'/login'}>
+              LOGIN
+            </Link>
+          )
+        }
+        <div className="hamburger">
+          <i className="fa-solid fa-bars"></i>
+        </div>
+      </div>
+    </div>
   )
 }
 
